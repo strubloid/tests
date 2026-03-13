0085d99c98be37ab7397c35013b109ec27bd3837
Strubloid <strubloid@gmail.com>
2026-03-13 14:41:45 +0000

    adding the base of tests

---
+++ 01-react/src/component/app/App.test.tsx
@@ -0,0 +1,14 @@
+// src/component/app/App.test.tsx
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+/**
+ * Basic smoke test to verify that the main app renders
+ * without throwing an error.
+ */
+describe("App", () => {
+    it("renders", () => {
+        render(<App />);
+        expect(screen.getByText("Get started")).toBeInTheDocument();
+    });
+});
