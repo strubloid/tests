40f8b3d17e4d43d1bd55325adae94be6860ec3c2
Strubloid <strubloid@gmail.com>
2026-03-17 15:21:42 +0000

    adding the service test 3

--- 03-react/src/component/products/ProductService.ts
+++ 03-react/src/component/products/ProductService.ts
@@ -11,7 +11,27 @@ export class ProductService {
      */
     public fetchData = async (url: string) => {
         try {
-            let data: any[] = [];
+            // getting the api fetch working
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            });
+
+            // checking if is a valid one
+            if (!response || !response.ok) {
+                throw new Error("Issues with the API, not able to get the data");
+            }
+
+            // we try to load the response
+            const data = response.json();
+
+            // we check if the respose is a good one
+            if (!data || (Array.isArray(data) && data.length <= 0)) {
+                throw new Error("Couldn't recieve data from the API");
+            }
 
             return data;
         } catch (error) {
@@ -21,12 +41,43 @@ export class ProductService {
     };
 
     /**
-     * This will be loading all product details
+     * This will be loading all product details, as this is a service
+     * we are sure to validate the main important ones, before show something
+     * that is missing data.
      * @param product
      */
     public loadDetails = (product: APIProduct): APIProduct | false => {
         try {
-            // after validations
+            // here we will be checking all the data in the product, only the main required one
+            // checking if we have the title
+            if (!product?.title) {
+                throw new Error("Missing the product title at the API");
+            }
+
+            // checking if we have an image
+            if (!product?.image) {
+                throw new Error("Missing the product Image at the API");
+            }
+
+            // checking if we have the description of the product
+            if (!product?.description) {
+                throw new Error("Missing the product Description at the API");
+            }
+
+            // checking if we have the price of the product
+            if (!product?.price) {
+                throw new Error("Missing the product Price at the API");
+            }
+
+            // checking if we have the category of the product
+            if (!product?.category) {
+                throw new Error("Missing the product Category at the API");
+            }
+
+            // checking if we have the raiting of the product
+            if (!product?.rating) {
+                throw new Error("Missing the product Raiting at the API");
+            }
 
             return product;
         } catch (error) {
