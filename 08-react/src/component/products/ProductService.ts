6a48e3350a4b65478fb8bfca003e3e043230b1b2
Strubloid <strubloid@gmail.com>
2026-03-19 16:18:06 +0000

    starting test 8 in a bit

---
+++ 08-react/src/component/products/ProductService.ts
@@ -0,0 +1,53 @@
+/**
+ * This class will be responsible for having all the rules
+ * related to the API calls, passing here as main point of
+ * request of data.
+ */
+export class ProductService {
+    /**
+     * This will return the data from the API server.
+     * @param url ulr of the place to get data from
+     * @returns data from the place or empty
+     */
+    public static fetchData = async (url: string) => {
+        try {
+            let data: APIProduct[] = [];
+
+            return data;
+        } catch (error) {
+            console.error("Error fetchin the data: ", error);
+            throw error;
+        }
+    };
+
+    /**
+     * This will be loading all product details
+     * @param product
+     */
+    public static loadDetails = (product: APIProduct): APIProduct => {
+        try {
+            // after validations
+
+            return product;
+        } catch (error) {
+            throw error;
+        }
+    };
+}
+
+// creating the APIProduct call type
+export interface APIProduct {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: APIProductRating;
+}
+
+// creating the API product Raiting type
+export interface APIProductRating {
+    rate: number;
+    count: number;
+}
